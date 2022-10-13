e64c21cfd1043259387da9167c6c5f686f2a7633
mohammadne <mohammadne.dev@gmail.com>
2022-10-13 22:34:03 +0330

    feat: update installation of the vim 2

--- scripts/install.sh
+++ scripts/install.sh
@@ -93,7 +93,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm --quiet
 # -------------------------------------------------------------------------- VIM
 
 echo "Installing & Activating vim-plug ..."
-curl -fLo ~/.config/vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PlugInstall +qa || echo "Something went wrong installing vim plugins."
 
 # -------------------------------------------------------------------------- NeoVIM
