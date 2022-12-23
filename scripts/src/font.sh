9ecc9f3dae8c5b5f056cb4df7d04b4c4f1ca9fb6
mohammadne <mohammadne.dev@gmail.com>
2022-12-23 12:34:57 +0330

    feat: add fonts

--- scripts/src/font.sh
+++ scripts/src/font.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+usage() {
+	echo -n "installs fonts"
+}
+
+install() {
+    msg "installing firacode-nerd"
+    require_pacman ttf-firacode-nerd
+}
+
+config() {
+    fc-cache -f ~/.local/share/fonts
+}
